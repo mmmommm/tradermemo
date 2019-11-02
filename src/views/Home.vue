b5c48a942248c02b3d8c0e8a2cc0c0707bec136a
ryota kise <ryota1839@gmail.com>
2019-11-02 13:02:50 +0900

    start

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,63 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <v-img src="@/assets/portfolio.jpg" height="770">
+        <v-container>
+            <v-layout justify-space-around wrap>
+                    <div v-for="(memo, index) in memos" :key="index">    
+                        <v-card width="350" height="150" class="my-5">
+                            <v-card-title class="ml-6">{{ memo.date }}</v-card-title>
+                            <v-card-text>
+                                <v-layout>
+                                    <v-btn outlined class="button ml-2 mt-6" :to="{ name: 'memo-detail', params: { memo: memo.slug }}">viewmemo</v-btn>
+                                    <!-- <v-btn outlined class="button ml-12 mt-6" @click="deleteMemo">deletememo</v-btn> -->
+                                </v-layout>
+                            </v-card-text>
+                        </v-card>
+                    </div>
+            </v-layout> 
+            <v-layout justify-center class="mt-10">
+                <v-btn to="/addMemo" x-large color="white">Add</v-btn>
+            </v-layout>
+        </v-container>
+    </v-img>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import db from "../../firebaseInit"
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
+    name:'home',
+    data(){
+        return {
+            memos: [],
+        }
+    },
+    created(){
+        db.collection('memos').get().then((querySnapshot) => {
+        const array = [];
+        querySnapshot.forEach((doc) => {
+            array.push(doc.data())
+        });
+        this.memos = array
+        });
+    },
+    // methods:{
+    //     deleteMemo: function(){
+    //         db.collection('memos').doc('memo.slug').delete().then(function(){
+    //             // eslint-disable-next-line no-console
+    //             console.log("document successfully deleted!");
+    //         }).catch(function(error){
+    //             // eslint-disable-next-line no-console
+    //             console.log("error removing document:", error);
+    //         });
+    //     }
+    // }
 }
 </script>
+<style scoped>
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .5s;
+}
+.fade-enter, .fade-leave-to {
+  opacity: 0;
+}
+</style>
